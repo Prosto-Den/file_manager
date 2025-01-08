45384b0f71b0d8c7185854890e54824cd9b8b9d5
Prosto_Den <comp1252@mail.ru>
2025-01-08 13:16:54 +0300

    Beta-2 + Исправление багов при переименовании

--- windows/renamewindow.py
+++ windows/renamewindow.py
@@ -9,14 +9,19 @@ class RenameWindow(wx.PopupTransientWindow):
         super().__init__(parent=parent, flags=flags)
         self.__entry = wx.TextCtrl(parent=self, style=wx.TE_PROCESS_ENTER)
         self.__filepath = filepath
-
         text = filepath.split('/')[-1]
-        if FileManipulator.is_file(self.__filepath):
-            suffix = FileManipulator.get_suffix(self.__filepath)
-            text = text.replace(suffix, '')
-
         self.__entry.AppendText(text)
-        self.__entry.SetSelection(0, len(text))
+        x, _ = self.__entry.GetTextExtent(text)
+        self.__entry.SetSize(wx.Size(x + 20, -1))
+
+        if FileManipulator.is_file(filepath):
+            for i in range(len(text) - 1, 0, -1):
+                if text[i] == '.':
+                    self.__entry.SetSelection(0, i)
+                    break
+        else:
+            self.__entry.SetSelection(0, len(text))
+
         self.__entry.SetFocus()
         self.SetPosition(pos)
         self.SetSize(self.__entry.GetSize())
@@ -27,10 +32,11 @@ class RenameWindow(wx.PopupTransientWindow):
     def OnDismiss(self) -> None:
         if not self.__entry.IsEmpty():
             new_filepath = self.__filepath.split('/')
-            if FileManipulator.is_file(self.__filepath):
-                file_suffix = FileManipulator.get_suffix(self.__filepath)
-            else:
-                file_suffix = ''
-            new_filepath.pop(-1)
-            new_filepath = '/'.join(new_filepath) + '/' + self.__entry.GetLineText(0) + file_suffix
+            old_file_name = new_filepath.pop(-1)
+            new_file_name = self.__entry.GetLineText(0)
+
+            if old_file_name == new_file_name:
+                return
+
+            new_filepath = '/'.join(new_filepath) + '/' + new_file_name
             FileManipulator.rename_file(self.__filepath, new_filepath)
